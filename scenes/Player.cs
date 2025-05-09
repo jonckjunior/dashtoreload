1002b3e68a117de7565c6ca29e387ef0bba8c39c
drek <kuodrek@gmail.com>
2025-05-08 23:39:21 -0300

    add dash code

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -1,10 +1,16 @@
 using Godot;
-using System;
 
 public partial class Player : Node2D {
     Vector2 velocity = Vector2.Zero;
     Vector2 carryOn = Vector2.Zero;
     Sprite2D HandSprite;
+
+    bool isPlayerDashing = false;
+    float dashLength = 100f;
+    Vector2 dashVelocity = Vector2.Zero;
+    Vector2 dashPositionStart = Vector2.Zero;
+    Vector2 dashPositionEnd = Vector2.Zero;
+    Vector2 dashDirection = Vector2.Zero;
     
 
     public override void _Ready() {
@@ -12,14 +18,27 @@ public partial class Player : Node2D {
     }
 
     public override void _Process(double delta) {
-        var direction = Input.GetVector("left", "right", "up", "down");
-        direction = direction.Normalized();
-        var targetSpeed = direction;
-        var acceleration = (float)0.1; // Adjust this value for more or less drift
-        velocity = velocity.Lerp(targetSpeed, acceleration);
+        if (!isPlayerDashing) {
+            var direction = Input.GetVector("left", "right", "up", "down");
+            direction = direction.Normalized();
+            var targetSpeed = direction;
+            var acceleration = (float)0.1; // Adjust this value for more or less drift
+            velocity = velocity.Lerp(targetSpeed, acceleration);
 
-        var positionBeforeRounding = Position + velocity + carryOn;
-        Position = positionBeforeRounding;
+            var positionBeforeRounding = Position + velocity + carryOn;
+            Position = positionBeforeRounding;
+        }
+        else {
+            var dashAcceleration = (float)0.5;
+            dashVelocity = dashVelocity.Lerp(dashDirection, dashAcceleration);
+            Position = Position + dashVelocity;
+
+            var tol = (float)0.1;
+            var diff = (GlobalPosition - dashPositionEnd).Length();
+            if (diff <= tol) {
+                ResetDashState();
+            }
+        }
 
         LookAt(GetGlobalMousePosition());
     
@@ -38,6 +57,13 @@ public partial class Player : Node2D {
                 AddSibling(bullet);
             }
         }
+        if (@event is InputEventKey keyEvent && keyEvent.Pressed && !keyEvent.Echo && !isPlayerDashing) {
+            switch (keyEvent.Keycode) {
+                case Key.Space:
+                    SetDashState();
+                    break;
+            }
+        }
     }
 
     private void DrawAimGuide(){
@@ -51,6 +77,21 @@ public partial class Player : Node2D {
         Color lineColor = Colors.White;
         lineColor.A = 0.5f; // Set the alpha value to 0.5 for transparency
         DrawLine(start, new Vector2(end, 0), lineColor, 2f); // Red line, 2 pixels wide
+    }
+
+    private void SetDashState() {
+        isPlayerDashing = true;
+
+        var cursorPosition = GetGlobalMousePosition();
+        var playerPosition = GlobalPosition;
+        dashDirection = (cursorPosition - playerPosition).Normalized();
+
+        dashPositionStart = GlobalPosition;
+        dashPositionEnd = dashPositionStart + dashDirection * dashLength;
+    }
+    private void ResetDashState() {
+        isPlayerDashing = false;
+        dashVelocity = Vector2.Zero;
     }
 
     public override void _Draw(){
